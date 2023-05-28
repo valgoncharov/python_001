6380541f545690bed42d05a660c65fd99985e2cc
goncharov.vv <goncharov.vv@esphere.ru>
2023-05-28 21:26:26 +0300

    First commit. try1

---
+++ main.py
@@ -0,0 +1,101 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Сложение
+number = 123 + 321
+
+# Вычитание
+number = 123 - 321
+
+# Деление
+number = 123 / 321
+
+# Целочисленное деление
+number = 123 // 321
+
+# Остаток от деления
+number = 123 % 321
+
+# Умножение
+number = 123 * 321
+
+# Возведение в степень
+number = 123 ** 321
+
+# Математические выражение выполняются по правилам арифметики
+number = (123 + 321) * 213
+
+# Присвоение результата сложения
+number += 1
+
+# Присвоение результата вычитания
+number -= 1
+
+# Присвоение результата умножения
+number *= 1
+
+# Присвоение результата деления
+number /= 1
+
+# Присвоение результата целочисленного деления
+number //=
+
+# Присвоение результата возведения числа в степень
+number **=
+
+# Присвоение результата остатка от деления
+number %=
+
+import math
+
+# Возведение числа X в степень Y
+math.pow(X, Y)
+
+# Факториал числа X
+math.factorial(X)
+
+# Модуль числа X
+math.fabs(X)
+
+# Округление X до ближайшего большего числа
+math.celi(X)
+
+# Округление X вниз
+math.floor(X)
+
+import random
+
+a = random.randint(1, 10)
+
+# Строки (Strings)
+# Строки в Python задаются одним из следующих образов:
+s = "Python"
+s = 'Python'
+s = """Python"""
+s = '''Python'''
+
+# Многострочные строки можно задавать следующим образом:
+multiline_string = """first
+second
+third"""
+
+multiline_string = "first\nsecond\nthird"
+
+multiline_string = "first\n" \
+                   "second\n" \
+                   "third"
+
+
--- test_first.py
+++ test_first.py
@@ -1,28 +1,13 @@
-from urllib import request
-
+import pytest
 
 def test_first():
     pass
 
-
-def test_first():
+def test_first1():
     assert 1 == 1
-
 
 def test_second():
     assert 1 == 2, "Единица не должна быть равна двойке"
-
-
-import pytest
-
-
-@pytest.fixture()
-def before_each():
-    print("Called before each test" + request.node.name)
-
-    def test_first1(before_each):
-        assert 1 == 1
-
 
 @pytest.fixture(scope='session', autouse=True)
 def before_all(request):
